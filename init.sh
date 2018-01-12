f7086f7fa23362b6c64707816fafaee2dbf73e6c
Nicolas BONNIOT <nicolas@devgiants.fr>
2018-01-12 15:21:20 +0100

    Fix #1

--- init.sh
+++ init.sh
@@ -22,33 +22,33 @@ envsubst < ./wp-cli.yml > ${WORDPRESS_HOST_RELATIVE_APP_PATH}/wp-cli.yml
 
 
 # Install WP
-docker-compose exec -u www-data php wp core download
+docker-compose exec --user www-data php wp core download
 
 # Create wp-config using wp-cli.yml file
-docker-compose exec -u www-data php wp config createdocker-cio
+docker-compose exec --user www-data php wp config create
 
-# Create database
-#docker-compose exec -u www-data php wp db create
+# Create database - not needed here because of docker-compose creation
+#docker-compose exec --user www-data php wp db create
 
 # Install site
-docker-compose exec -u www-data php wp core install
+docker-compose exec --user www-data php wp core install
 
 # TODO Install ACF pro https://github.com/wp-premium/advanced-custom-fields-pro/archive/master.zip
 
 # install plugins
 while read line
 do
-    docker-compose exec -u www-data php wp plugin install $line --activate
+    docker-compose exec --user www-data php wp plugin install $line --activate
 done < ./plugins.txt
 
 # Clean tedious elements
-docker-compose exec -u www-data php wp post delete 1 --force
-docker-compose exec -u www-data php wp post delete 2 --force
-docker-compose exec -u www-data php wp plugin delete hello
+docker-compose exec --user www-data php wp post delete 1 --force
+docker-compose exec --user www-data php wp post delete 2 --force
+docker-compose exec --user www-data php wp plugin delete hello
 
 # Set permalinks to postname
-docker-compose exec -u www-data php wp rewrite structure "/%postname%/" --hard
-docker-compose exec -u www-data php wp rewrite flush --hard
+docker-compose exec --user www-data php wp rewrite structure "/%postname%/" --hard
+docker-compose exec --user www-data php wp rewrite flush --hard
 
 # Install Roots Sage
-docker-compose exec -u www-data php composer create-project roots/sage ${PROJECT_NAME} 8.5.3
+docker-compose exec --user www-data php composer create-project roots/sage ${PROJECT_NAME} 8.5.3
